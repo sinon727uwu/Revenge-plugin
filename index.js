7d21263bfba2cb63e6c63bde437acd52da70f7b6
sinon727uwu <AnimeWaifuGirl@protonmail.com>
2026-07-20 11:52:53 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default {
+    onLoad: () => {
+        console.log("CommandExecutor Plugin geladen!");
+    },
+    onUnload: () => {
+        console.log("CommandExecutor Plugin entladen!");
+    }
+};
